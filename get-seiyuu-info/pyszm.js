6afb81449f44f50507c5abcfd0ce555cd8134b4b
nulla2011 <nulla@outlook.com>
2022-04-14 10:47:59 +0800

    rename

--- get-seiyuu-info/pyszm.js
+++ get-seiyuu-info/pyszm.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.pysx = void 0;
+exports.pyszm = void 0;
 const pinyin_1 = __importDefault(require("pinyin"));
 const lodash_1 = __importDefault(require("lodash"));
-function pysx(text) {
+function pyszm(text) {
     return (0, pinyin_1.default)(text).reduce((p, c) => p = p + lodash_1.default.deburr(c[0][0]), '');
 }
-exports.pysx = pysx;
-console.log(pysx(process.argv[2]));
+exports.pyszm = pyszm;
+console.log(pyszm(process.argv[2]));
